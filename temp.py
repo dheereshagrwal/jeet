9f3c2cc43a94d622b06a8730a9e35f22e245a826
desitrader <desitrader2020@gmail.com>
2023-01-12 22:19:38 -0500

    Removed Temp

--- temp.py
+++ temp.py
@@ -1,13 +1,2 @@
-# convert 2023-01-09 to milliseconds at 9.30.00
-import pandas as pd
-
-
-def get_timestamp(date, time):
-    date = date + " "+time
-    date = pd.to_datetime(date)
-    date = date.timestamp()
-    date = date * 1000
-    return int(date)
-
 
 
